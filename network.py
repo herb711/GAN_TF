95ba4f1d01c2ebd785b26f2518d33d246e489f58
zhushiyu <38155482+herb711@users.noreply.github.com>
2018-11-27 22:21:54 +0800

    test2

---
+++ network.py
@@ -0,0 +1,231 @@
+'''
+构建模型
+'''
+
+import tensorflow as tf
+
+# # 构建模型
+# 
+# - inputs
+# - generator
+# - discriminator
+# - loss
+# - optimizer
+
+# ## Inputs
+
+# In[6]:
+
+
+def get_inputs(noise_dim, image_height, image_width, image_depth):
+    """
+    @Author: zhushiyu
+    --------------------
+    :param noise_dim: 噪声图片的size
+    :param image_height: 真实图像的height
+    :param image_width: 真实图像的width
+    :param image_depth: 真实图像的depth
+    """ 
+    inputs_real = tf.placeholder(tf.float32, [None, image_height, image_width, image_depth], name='inputs_real')
+    inputs_noise = tf.placeholder(tf.float32, [None, noise_dim], name='inputs_noise')
+    
+    return inputs_real, inputs_noise
+
+
+# # Generator
+
+# In[7]:
+
+
+def get_generator(noise_img, output_dim, is_train=True, alpha=0.01):
+    """
+    @Author: zhushiyu
+    --------------------
+    :param noise_img: 噪声信号，tensor类型
+    :param output_dim: 生成图片的depth
+    :param is_train: 是否为训练状态，该参数主要用于作为batch_normalization方法中的参数使用
+    :param alpha: Leaky ReLU系数
+    """
+    
+    with tf.variable_scope("generator", reuse=(not is_train)):
+        # 100 x 1 to 8 x 8 x 512
+        # 全连接层
+        layer1 = tf.layers.dense(noise_img, 8*8*512)
+        layer1 = tf.reshape(layer1, [-1, 8, 8, 512])
+        # batch normalization
+        layer1 = tf.layers.batch_normalization(layer1, training=is_train)
+        # Leaky ReLU
+        layer1 = tf.maximum(alpha * layer1, layer1)
+        # dropout
+        layer1 = tf.nn.dropout(layer1, keep_prob=0.8)
+        
+        # 8 x 8 x 512 to 16 x 16 x 256
+        layer2 = tf.layers.conv2d_transpose(layer1, 256, 3, strides=2, padding='same')
+        layer2 = tf.layers.batch_normalization(layer2, training=is_train)
+        layer2 = tf.maximum(alpha * layer2, layer2)
+        layer2 = tf.nn.dropout(layer2, keep_prob=0.8)
+        
+        # 16 x 16 x 256 to 32 x 32 x 128
+        layer3 = tf.layers.conv2d_transpose(layer2, 128, 3, strides=2, padding='same')
+        layer3 = tf.layers.batch_normalization(layer3, training=is_train)
+        layer3 = tf.maximum(alpha * layer3, layer3)
+        layer3 = tf.nn.dropout(layer3, keep_prob=0.8)
+        
+        # 32 x 32 x 128 to 64 x 64 x 1
+        logits = tf.layers.conv2d_transpose(layer3, output_dim, 3, strides=2, padding='same')
+        
+        outputs = tf.tanh(logits)
+
+        return outputs
+
+
+# ## Discriminator
+
+# In[8]:
+
+
+def get_discriminator(inputs_img, reuse=False, alpha=0.01):
+    """
+    @Author: zhushiyu
+    --------------------
+    @param inputs_img: 输入图片，tensor类型
+    @param alpha: Leaky ReLU系数
+    """
+    
+    with tf.variable_scope("discriminator", reuse=reuse):
+        # 64 x 64 x 1 to 32 x 32 x 128
+        # 第一层不加入BN
+        layer1 = tf.layers.conv2d(inputs_img, filters=128, kernel_size=3, strides=2, padding='same')
+        layer1 = tf.maximum(alpha * layer1, layer1) # Leaky ReLU
+        layer1 = tf.nn.dropout(layer1, keep_prob=0.8)
+        
+        # 32 x 32 x 128 to 16 x 16 x 256
+        layer2 = tf.layers.conv2d(layer1, 256, 3, strides=2, padding='same')
+        layer2 = tf.layers.batch_normalization(layer2, training=True)
+        layer2 = tf.maximum(alpha * layer2, layer2)
+        layer2 = tf.nn.dropout(layer2, keep_prob=0.8)
+        
+        # 16 x 16 x 256 to 8 x 8 x 512
+        layer3 = tf.layers.conv2d(layer2, 512, 3, strides=2, padding='same')
+        layer3 = tf.layers.batch_normalization(layer3, training=True)
+        layer3 = tf.maximum(alpha * layer3, layer3)
+        layer3 = tf.nn.dropout(layer3, keep_prob=0.8)
+        
+        # 8 x 8 x 512 to 8*8*512 x 1
+        flatten = tf.reshape(layer3, (-1, 8*8*512))
+        logits = tf.layers.dense(flatten, 1)
+        outputs = tf.sigmoid(logits)
+        
+        return logits, outputs
+
+
+# ## Loss
+
+# In[9]:
+
+
+def get_loss(inputs_real, inputs_noise, image_depth, smooth=0.05):
+    """
+    @Author: zhushiyu
+    --------------------
+    @param inputs_real: 输入图片，tensor类型
+    @param inputs_noise: 噪声图片，tensor类型
+    @param image_depth: 图片的depth（或者叫channel）
+    @param smooth: label smoothing的参数
+    """
+    
+    g_outputs = get_generator(inputs_noise, image_depth, is_train=True)
+    d_logits_real, d_outputs_real = get_discriminator(inputs_real)
+    d_logits_fake, d_outputs_fake = get_discriminator(g_outputs, reuse=True)
+     
+    
+    # 计算Loss
+    g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, 
+                                                                    labels=tf.ones_like(d_outputs_fake)*(1-smooth))) # ones_like全部值置为1
+    
+    d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_real,
+                                                                         labels=tf.ones_like(d_outputs_real)*(1-smooth)))
+    d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake,
+                                                                         labels=tf.zeros_like(d_outputs_fake)))
+    d_loss = tf.add(d_loss_real, d_loss_fake)
+    
+    return g_loss, d_loss
+
+
+# ## Optimizer
+
+# In[10]:
+
+
+def get_optimizer(g_loss, d_loss, beta1=0.4, learning_rate=0.001):
+    """
+    利用梯度下降法对网络进行优化
+    @Author: zhushiyu
+    --------------------
+    @param g_loss: Generator的Loss
+    @param d_loss: Discriminator的Loss
+    @learning_rate: 学习率
+    """
+    
+    train_vars = tf.trainable_variables()
+    
+    g_vars = [var for var in train_vars if var.name.startswith("generator")]
+    d_vars = [var for var in train_vars if var.name.startswith("discriminator")]
+    
+    # Optimizer
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        g_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(g_loss, var_list=g_vars)
+        d_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(d_loss, var_list=d_vars)
+    
+    return g_opt, d_opt
+
+
+# ## 增加的模型
+
+# In[11]:
+
+
+def get_opt_z(noise_size, data_shape, inputs_real, mask_np, learning_rate=0.001) :
+    """
+    @Author: zhushiyu
+    --------------------
+    @param noise_size: 噪声size
+    @param data_shape: 真实图像shape
+    @batch_size:
+    @n_samples: 显示示例图片数量
+    
+    # 寻找与待修复图像最接近的伪图像 
+    """
+    
+    # 生成器的输入
+    z = tf.random_uniform([noise_size], -1.0, 1.0)
+    var_z = tf.Variable(initial_value=z, name='batch_z', dtype=tf.float32)
+    layer_z = tf.reshape(var_z, [1, noise_size])
+
+    # 生成屏蔽器
+    mask_np_ = mask_np.reshape(1, data_shape[1], data_shape[2], data_shape[3])
+    mask = tf.constant(value=mask_np_, dtype=tf.float32)
+
+    # 模型结构
+    z_images = get_generator(layer_z, data_shape[-1], False) # in (1x100)  out (1x64x64x1)
+    z_logits_fake, z_outputs_fake = get_discriminator(z_images, reuse=True) # in (1x64x64x1)  out (1)
+    z_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=z_logits_fake, 
+                                                                    labels=tf.ones_like(z_outputs_fake)*(1-0.1))) # 计算Loss
+    
+    b_img = tf.reshape(inputs_real[0], [1, data_shape[1], data_shape[2], 1]) 
+    contextual0 = tf.multiply(mask, z_images) - tf.multiply(mask, b_img) # mask (1x64x64x1)  z_images (1x64x64x1)  b_img (1x64x64x1)
+    contextual1 =  tf.contrib.layers.flatten( tf.abs( contextual0 ) )
+    contextual_loss = tf.reduce_sum( contextual1, 1 ) # 伪图像跟真实图像接近情况    
+    
+    perceptual_loss = z_loss # 判别器认定为真的情况
+    complete_loss = contextual_loss + 0.99 * perceptual_loss
+    grad_z = tf.gradients(complete_loss, var_z) 
+    learn_z = tf.constant(value=learning_rate, dtype=tf.float32) # 设置学习率
+    z_opt = var_z.assign(var_z - learn_z * tf.reshape(grad_z, [noise_size])) # 将 1x100 转换为 100
+    
+    return z_opt, layer_z, complete_loss
+
+
+    
+    
+
---
+++ train.py
@@ -0,0 +1,179 @@
+
+'''
+初始化模型
+训练模型
+'''
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import pickle
+
+import loader
+import network as net
+
+
+
+# 定义模型参数
+learning_rate = 0.001
+beta1 = 0.4
+
+# 定义输入输出参数
+noise_size = 100
+data_shape = [-1, 64, 64, 1]
+
+# 定义训练参数
+epochs = 1
+batch_size = 1
+n_samples = 101
+
+# 生成屏蔽区域
+m = 20
+n = 40
+mask_np = np.ones([data_shape[1],data_shape[2]]) #生成一个64x64的全1矩阵
+mask_np[m:n, m:n]= 0
+
+mask_np0 = np.zeros([data_shape[1],data_shape[2]]) #生成一个64x64的全0矩阵
+mask_np0[m:n, m:n]= 1
+
+
+
+
+def plot_images(samples):
+    fig, axes = plt.subplots(nrows=1, ncols=25, sharex=True, sharey=True, figsize=(50,2))
+    for img, ax in zip(samples, axes):
+        ax.imshow(img.reshape((64, 64)), cmap='Greys_r')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    fig.tight_layout(pad=0)
+    plt.show()
+
+
+def show_generator_output(sess, examples_noise, inputs_noise, output_dim):
+    """
+    @Author: zhushiyu
+    --------------------
+    @param sess: TensorFlow session
+    @param n_images: 展示图片的数量
+    @param inputs_noise: 噪声图片
+    @param output_dim: 图片的depth（或者叫channel）
+    @param image_mode: 图像模式：RGB或者灰度
+    """
+
+    # 生成噪声图片
+    samples = sess.run(net.get_generator(inputs_noise, output_dim, False),
+                       feed_dict={inputs_noise: examples_noise})
+
+    result = np.squeeze(samples, -1)
+    return result
+
+
+def train(data_train):
+    """
+    @Author: zhushiyu
+    --------------------
+    @param noise_size: 噪声size
+    @param data_shape: 真实图像shape
+    @epochs:           训练次数
+    @batch_size:       小批量大小
+    @n_samples:        间隔显示的频率
+    """
+
+    # 存储loss
+    losses = []
+    steps = 0
+    
+    inputs_real, inputs_noise = net.get_inputs(noise_size, data_shape[1], data_shape[2], data_shape[3])
+    g_loss, d_loss = net.get_loss(inputs_real, inputs_noise, data_shape[-1])
+    g_train_opt, d_train_opt = net.get_optimizer(g_loss, d_loss, beta1, learning_rate)
+
+    z_train_opt, z_img, z_loss = net.get_opt_z(noise_size, data_shape, inputs_real, mask_np, learning_rate)
+      
+    # 保存生成器变量
+    saver = tf.train.Saver()
+    
+    
+    # 读取png版本
+    test_img = random.sample(data_train, batch_size)
+    y = test_img[0]
+    test_img = [y for x in test_img]
+    test_img = np.array(test_img)
+    test_img = test_img.reshape((batch_size, data_shape[1], data_shape[2], data_shape[3]))
+    plot_images(np.squeeze(test_img, -1)) # 显示完整图像
+
+    # 生成待修复图像
+    mask_np_ = mask_np.reshape(data_shape[1], data_shape[2], data_shape[3])
+    batch_mix = [np.multiply(mask_np_, y) for y in test_img] # 点乘
+    plot_images(np.squeeze(batch_mix, -1)) # 显示残缺图像    
+
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        # 迭代epoch
+        for e in range(epochs):
+            for batch_i in range(30000//batch_size): ##(mnist.train.num_examples//batch_size):
+                steps += 1
+                
+                # minst版本
+                #batch = mnist.train.next_batch(batch_size)
+                #batch_images = batch[0].reshape((batch_size, data_shape[1], data_shape[2], data_shape[3]))
+                
+                # 读取csv版本
+                #batch = data_train.sample(batch_size)
+                #batch = batch.as_matrix()   
+                
+                # 读取png版本
+                batch = random.sample(data_train, batch_size)
+                batch = np.array(batch)               
+                batch_images = batch.reshape((batch_size, data_shape[1], data_shape[2], data_shape[3]))
+                
+                # scale to -1, 1
+                #batch_images = batch_images * 2 - 1
+
+                # noise
+                batch_noise = np.random.uniform(-1, 1, size=(batch_size, noise_size))
+
+                # run optimizer
+                _ = sess.run(g_train_opt, feed_dict={inputs_real: batch_images,
+                                                     inputs_noise: batch_noise})
+                _ = sess.run(d_train_opt, feed_dict={inputs_real: batch_images,
+                                                     inputs_noise: batch_noise})
+                
+                if steps % n_samples == 0:
+                    train_loss_d = d_loss.eval({inputs_real: batch_images,
+                                                inputs_noise: batch_noise})
+                    train_loss_g = g_loss.eval({inputs_real: batch_images,
+                                                inputs_noise: batch_noise})
+                    
+                    _, train_img_z, train_loss_z = sess.run([z_train_opt, z_img, z_loss], 
+                                                   feed_dict={inputs_real: batch_mix})
+                    
+                    losses.append((train_loss_d, train_loss_g))
+                    # 显示图片
+                    samples = show_generator_output(sess, batch_noise, inputs_noise, data_shape[-1])
+                    plot_images(samples)
+
+                    samples = show_generator_output(sess, train_img_z, inputs_noise, data_shape[-1])
+                    plot_images(samples)
+
+                    
+                    print("Epoch {}/{}....".format(e+1, epochs), 
+                          "Discriminator Loss: {:.4f}....".format(train_loss_d),
+                          "Generator Loss: {:.4f}....". format(train_loss_g),
+                          "Z Loss: {:.4f}....". format(np.mean(train_loss_z)))
+                    saver.save(sess, './checkpoints/generator.ckpt')
+    return losses
+
+if __name__=="__main__":
+    data_train, data_test = loader.read_yizu()
+    with tf.Graph().as_default():
+        losses = train(data_train)
+        with open('train_losses.pkl', 'wb') as f:
+            pickle.dump(losses, f)
+            f.close()
+
+
+    
+    
+
